dd51f6c315a91afd975cbb80f7ecc6f99c2502ad
Marcel Peruch <mperuch@gmail.com>
2018-09-13 20:00:30 -0300

    Config .env

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
+  dialect: 'mysql',
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_DATABASE,
   host: process.env.DB_HOST,
-  dialect: 'mysql',
 };
